3e55723ac18f0ce9552f69d641d74768bf76347b
Sofia1Souza <souzaalvarezsofia@gmail.com>
2023-05-18 20:28:28 -0300

    practicando querys

--- PracticasSql.sql
+++ PracticasSql.sql
@@ -1,3 +1,5 @@
+
+-- create
 CREATE DATABASE practicaSql;
 
  USE practicaSql;
@@ -26,5 +28,26 @@ INSERT INTO usuarios (idUsuarios,Nombre,Apellido,Celular,Correo,Clave) VALUES
 (8,'Julia','Lisboa','1598435610','julial@sql.com','julial113#'),
 (9,'Hernan','Piquin','1565209861','hernanp@sql.com','hernanp778!'),
 (10,'Camila','Hurt','1531086340','camilah@sql.com','camilac554&');
+
+-- ingresando datos sin especificar values
+INSERT INTO usuarios VALUES
+(11,'Lionel','Messi','1512456730','liom@seleccion.com','liom***3c'),
+(12,'Julian','Araña','1523456717','juliara@seleccion.com','juliara3c***');
+
+-- practicando sentencias(Read)
+
+SELECT *FROM usuarios; -- trae todos los datos de la tabla
+
+SELECT COUNT(Nombre) FROM usuarios; -- cuenta el total de nombres
+
+SELECT Nombre,Apellido FROM usuarios; -- trae las columnas especificadas
+
+SELECT Nombre FROM usuarios WHERE Nombre LIKE "j%"; -- Muestra los usuarios cuyo nombre empiezan con j
+
+SELECT Nombre,Correo,idUsuarios FROM usuarios WHERE idUsuarios BETWEEN 7 AND 12; -- Muestra las columnas pedidas dentro del rango especificado de id
+
+SELECT Nombre,Correo,idUsuarios FROM usuarios WHERE idUsuarios >=5; -- Muestra las columnas pedidas de los id iguales que 5 para arriba
+
+SELECT Nombre FROM usuarios ORDER BY Nombre ASC; -- Ordena alfabeticamente los nombres en este caso.
 
 
